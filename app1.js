20a76f8a8b0d2cab1be5161d41df8b8e6ff7b185
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-01-06 16:12:32 -0800

    added 3 js functions to the page

---
+++ app1.js
@@ -0,0 +1,5 @@
+let userName = window.prompt('Please Enter Your Name');
+
+document.log (userName)
+
+console.log('here')
---
+++ app2.js
@@ -0,0 +1,5 @@
+if (userName == 'Lydia'){
+    document.write('<head>' + 'Welcome Lydia' + '</head>');
+} else {
+    document.write('<head>'  + 'Welcome Kitty Lover' + '</head>');
+}
